a9be5b65752a82e9d7be5120c3db40b56b4d4a9b
Mukai Selekwa <mukaiss@outlook.com>
2020-01-01 16:52:22 -0600

    Create network_status_page.dart

---
+++ lib/home_pages/network_status_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:webblen/styles/fonts.dart';
+import 'package:webblen/styles/flat_colors.dart';
+import 'package:webblen/widgets_common/common_button.dart';
+
+class NetworkStatusPage extends StatelessWidget {
+
+  final VoidCallback reloadAction;
+  NetworkStatusPage({this.reloadAction});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Container(
+          constraints: BoxConstraints(
+            maxWidth: 300,
+          ),
+          child: Fonts().textW500("You are not connected to the Internet.", 16.0, FlatColors.darkGray, TextAlign.center),
+        ),
+        SizedBox(height: 8.0),
+        CustomColorButton(
+          text: 'Try Again',
+          textColor: FlatColors.darkGray,
+          backgroundColor: Colors.white,
+          height: 45.0,
+          width: 100.0,
+          hPadding: 16.0,
+          onPressed: reloadAction,
+        )
+      ],
+    );
+  }
+}
