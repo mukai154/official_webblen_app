6ea75fa426c8fe4475aabb1e32bd7ad1a4913567
Mukai Selekwa <mukaiss@outlook.com>
2020-01-01 16:51:30 -0600

    Update home_dashboard_page.dart

--- lib/home_pages/home_dashboard_page.dart
+++ lib/home_pages/home_dashboard_page.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:webblen/services_general/service_page_transitions.dart';
+import 'package:webblen/firebase_data/event_data.dart';
+import 'package:webblen/firebase_data/user_data.dart';
+import 'package:webblen/models/event.dart';
+import 'package:webblen/models/local_ad.dart';
 import 'package:webblen/models/webblen_user.dart';
+import 'package:webblen/services_general/service_page_transitions.dart';
 import 'package:webblen/services_general/services_show_alert.dart';
-import 'package:webblen/widgets_home_tiles/all_tiles.dart';
 import 'package:webblen/styles/flat_colors.dart';
 import 'package:webblen/styles/fonts.dart';
-import 'package:webblen/firebase_data/ad_data.dart';
-import 'package:webblen/models/local_ad.dart';
-import 'package:webblen/firebase_data/user_data.dart';
 import 'package:webblen/widgets_common/common_progress.dart';
-import 'package:webblen/models/event.dart';
-import 'package:webblen/widgets_home_tiles/webblen_events_tile.dart';
-import 'package:webblen/firebase_data/event_data.dart';
 import 'package:webblen/widgets_event/event_carousel.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:webblen/widgets_home_tiles/all_tiles.dart';
+import 'package:webblen/widgets_home_tiles/webblen_events_tile.dart';
 
 class HomeDashboardPage extends StatefulWidget {
-
   final WebblenUser currentUser;
   final bool updateRequired;
   final String areaName;
@@ -33,7 +30,6 @@ class HomeDashboardPage extends StatefulWidget {
 }
 
 class _HomeDashboardPageState extends State<HomeDashboardPage> {
-
   List<LocalAd> ads = [];
   List<WebblenUser> randomNearbyUsers = [];
   List<Event> webblenEvents = [];
@@ -41,81 +37,76 @@ class _HomeDashboardPageState extends State<HomeDashboardPage> {
   String nearbyUserCount;
   bool isLoading = true;
 
-  bool updateAlertIsEnabled(){
+  bool updateAlertIsEnabled() {
     bool showAlert = false;
-    if (widget.updateRequired){
+    if (widget.updateRequired) {
       showAlert = true;
     }
     return showAlert;
   }
 
-  loadData(){
-    //GET nearby ads
-    AdDataService().getNearbyAds(widget.currentLat, widget.currentLon).then((res){
-      ads = res;
-      //GET Webblen Events
-      EventDataService().getExclusiveWebblenEvents().then((res){
-        webblenEvents = res;
-        //GET Recommended Events
-        EventDataService().getRecommendedEvents(widget.currentUser.uid, widget.areaName).then((res){
-          recommendedEvents = res;
-          //GET Number of Nearby Users
-          UserDataService().getNumberOfNearbyUsers(widget.currentLat, widget.currentLon).then((res){
-            nearbyUserCount = res;
-            if (this.mounted){
-              isLoading = false;
-              setState(() {});
-            }
-          });
+  loadData() {
+    EventDataService().getExclusiveWebblenEvents().then((res) {
+      webblenEvents = res;
+      //GET Recommended Events
+      EventDataService().getRecommendedEvents(widget.currentUser.uid, widget.areaName).then((res) {
+        recommendedEvents = res;
+        //GET Number of Nearby Users
+        UserDataService().getNumberOfNearbyUsers(widget.currentLat, widget.currentLon).then((res) {
+          nearbyUserCount = res;
+          if (this.mounted) {
+            isLoading = false;
+            setState(() {});
+          }
         });
       });
     });
   }
 
-  void didPressDiscoverTile(){
-    if (widget.currentUser != null && !widget.updateRequired){
+  void didPressDiscoverTile() {
+    if (widget.currentUser != null && !widget.updateRequired) {
       PageTransitionService(context: context, uid: widget.currentUser.uid, areaName: widget.areaName).transitionToDiscoverPage();
-    } else if (updateAlertIsEnabled()){
+    } else if (updateAlertIsEnabled()) {
       ShowAlertDialogService().showUpdateDialog(context);
     }
   }
 
-  void didPressSearchTile(){
-    if (widget.currentUser != null && !widget.updateRequired){
+  void didPressSearchTile() {
+    if (widget.currentUser != null && !widget.updateRequired) {
       PageTransitionService(context: context, currentUser: widget.currentUser, areaName: widget.areaName).transitionToSearchPage();
-    } else if (updateAlertIsEnabled()){
+    } else if (updateAlertIsEnabled()) {
       ShowAlertDialogService().showUpdateDialog(context);
     }
   }
 
-  void didPressMyCommunitiesTile(){
-    if (widget.currentUser != null && !widget.updateRequired){
+  void didPressMyCommunitiesTile() {
+    if (widget.currentUser != null && !widget.updateRequired) {
       PageTransitionService(context: context, uid: widget.currentUser.uid, areaName: widget.areaName).transitionToMyCommunitiesPage();
-    } else if (updateAlertIsEnabled()){
+    } else if (updateAlertIsEnabled()) {
       ShowAlertDialogService().showUpdateDialog(context);
     }
   }
 
-  void didPressCommunityRequestTile(){
-    if (widget.currentUser != null && !widget.updateRequired){
+  void didPressCommunityRequestTile() {
+    if (widget.currentUser != null && !widget.updateRequired) {
       PageTransitionService(context: context, currentUser: widget.currentUser, areaName: widget.areaName).transitionToCommunityRequestPage();
-    } else if (updateAlertIsEnabled()){
+    } else if (updateAlertIsEnabled()) {
       ShowAlertDialogService().showUpdateDialog(context);
     }
   }
 
-  void didPressCommunityActivityTile(){
-    if (widget.currentUser != null && !widget.updateRequired){
+  void didPressCommunityActivityTile() {
+    if (widget.currentUser != null && !widget.updateRequired) {
       PageTransitionService(context: context, currentUser: widget.currentUser).transitionToUserRanksPage();
-    } else if (updateAlertIsEnabled()){
+    } else if (updateAlertIsEnabled()) {
       ShowAlertDialogService().showUpdateDialog(context);
     }
   }
 
-  void didPressWebblenEventsTile(){
-    if (widget.currentUser != null && !widget.updateRequired){
+  void didPressWebblenEventsTile() {
+    if (widget.currentUser != null && !widget.updateRequired) {
       PageTransitionService(context: context, currentUser: widget.currentUser, events: webblenEvents).transitionToWebblenEventsPage();
-    } else if (updateAlertIsEnabled()){
+    } else if (updateAlertIsEnabled()) {
       ShowAlertDialogService().showUpdateDialog(context);
     }
   }
@@ -140,44 +131,28 @@ class _HomeDashboardPageState extends State<HomeDashboardPage> {
               children: [
                 Expanded(
                   flex: 4,
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      MediaQuery(
-                        data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
-                        child: widget.areaName.length <= 6
-                            ? Fonts().textW700(widget.areaName, 40, Colors.black, TextAlign.left)
-                            : widget.areaName.length <= 8
-                            ? Fonts().textW700(widget.areaName, 35, Colors.black, TextAlign.left)
-                            : widget.areaName.length <= 10
-                            ? Fonts().textW700(widget.areaName, 30, Colors.black, TextAlign.left)
-                            : widget.areaName.length <= 12
-                            ? Fonts().textW700(widget.areaName, 25, Colors.black, TextAlign.left)
-                            : Fonts().textW700(widget.areaName, 20, Colors.black, TextAlign.left),
-                      ),
-                    ]
-                  ),
+                  child: Column(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.start, children: [
+                    MediaQuery(
+                      data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+                      child: widget.areaName.length <= 6
+                          ? Fonts().textW700(widget.areaName, 40, Colors.black, TextAlign.left)
+                          : widget.areaName.length <= 8
+                              ? Fonts().textW700(widget.areaName, 35, Colors.black, TextAlign.left)
+                              : widget.areaName.length <= 10
+                                  ? Fonts().textW700(widget.areaName, 30, Colors.black, TextAlign.left)
+                                  : widget.areaName.length <= 12
+                                      ? Fonts().textW700(widget.areaName, 25, Colors.black, TextAlign.left)
+                                      : Fonts().textW700(widget.areaName, 20, Colors.black, TextAlign.left),
+                    ),
+                  ]),
                 ),
                 Expanded(
                   flex: 1,
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        widget.notifWidget
-                      ]
-                  ),
+                  child: Column(mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.end, children: [widget.notifWidget]),
                 ),
                 Expanded(
                   flex: 1,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        widget.accountWidget
-                      ]
-                  ),
+                  child: Column(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.end, children: [widget.accountWidget]),
                 ),
               ],
             ),
@@ -197,10 +172,8 @@ class _HomeDashboardPageState extends State<HomeDashboardPage> {
                 )
               : Container(
                   height: MediaQuery.of(context).size.height > 667.0
-                    ? MediaQuery.of(context).size.height * 0.715
-                    : MediaQuery.of(context).size.height > 568.0
-                    ? MediaQuery.of(context).size.height * 0.67
-                    : MediaQuery.of(context).size.height * 0.60,
+                      ? MediaQuery.of(context).size.height * 0.715
+                      : MediaQuery.of(context).size.height > 568.0 ? MediaQuery.of(context).size.height * 0.67 : MediaQuery.of(context).size.height * 0.60,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
@@ -209,9 +182,7 @@ class _HomeDashboardPageState extends State<HomeDashboardPage> {
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
-                            DiscoverTile(
-                                onTap: () => didPressDiscoverTile()
-                            ),
+                            DiscoverTile(onTap: () => didPressDiscoverTile()),
                           ],
                         ),
                       ),
@@ -235,20 +206,20 @@ class _HomeDashboardPageState extends State<HomeDashboardPage> {
                           Container(
                             //width: MediaQuery.of(context).size.width/1.5,
                             child: Padding(
-                                padding: EdgeInsets.only(left: 16.0, top: 16.0, bottom: 8.0),
-                                child: MediaQuery(
-                                    data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
-                                    child: Fonts().textW700('Events You Might Like', 18.0, Colors.black, TextAlign.left),
-                                ),
+                              padding: EdgeInsets.only(left: 16.0, top: 16.0, bottom: 8.0),
+                              child: MediaQuery(
+                                data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+                                child: Fonts().textW700('Events You Might Like', 18.0, Colors.black, TextAlign.left),
+                              ),
                             ),
                           ),
                         ],
                       ),
                       webblenEvents.isEmpty
-                          ?  recommendedEvents.isEmpty ? Container() : EventCarousel(events: recommendedEvents, currentUser: widget.currentUser)
+                          ? recommendedEvents.isEmpty ? Container() : EventCarousel(events: recommendedEvents, currentUser: widget.currentUser)
                           : WebblenEventsTile(
-                        onTap: () => didPressWebblenEventsTile(),
-                      ),
+                              onTap: () => didPressWebblenEventsTile(),
+                            ),
                     ],
                   ),
                 ),
@@ -257,62 +228,3 @@ class _HomeDashboardPageState extends State<HomeDashboardPage> {
     );
   }
 }
-
-
-//Column(
-//children: <Widget>[
-//Expanded(
-//child: Padding(
-//padding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 16.0),
-//child: Row(
-//mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//children: <Widget>[
-//DiscoverTile(
-//onTap: () => didPressDiscoverTile()
-//),
-//],
-//),
-//),
-//),
-//Expanded(
-//child: Padding(
-//padding: EdgeInsets.symmetric(horizontal: 16.0),
-//child: Row(
-//mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//children: <Widget>[
-//MyCommunitiesTile(
-//onTap: () => didPressMyCommunitiesTile(),
-//),
-//CommunityRequestTile(
-//onTap: () => didPressCommunityRequestTile(),
-//),
-//],
-//),
-//),
-//),
-//Expanded(
-//child: Row(
-//children: <Widget>[
-//Container(
-//width: MediaQuery.of(context).size.width/2,
-//child: Padding(
-//padding: EdgeInsets.only(left: 16.0, top: 16.0, bottom: 8.0),
-//child: FittedBox(
-//fit: BoxFit.scaleDown,
-//child: Fonts().textW700('Events You Might Like', 18.0, Colors.black, TextAlign.left),
-//)
-//),
-//),
-//],
-//),
-//),
-//Expanded(
-//child: webblenEvents.isEmpty
-//? recommendedEvents.isEmpty ? Container() : EventCarousel(events: recommendedEvents)
-//: WebblenEventsTile(
-//onTap: () => didPressWebblenEventsTile(),
-//),
-//),
-//
-//],
-//),
