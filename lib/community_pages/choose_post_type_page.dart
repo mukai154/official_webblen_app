d7de5497809161473c3e60806c9e15b421158dd5
Mukai Selekwa <mukaiss@outlook.com>
2020-01-01 16:36:38 -0600

    Update choose_post_type_page.dart

--- lib/community_pages/choose_post_type_page.dart
+++ lib/community_pages/choose_post_type_page.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:webblen/styles/fonts.dart';
-import 'package:webblen/styles/flat_colors.dart';
-import 'package:webblen/widgets_common/common_button.dart';
-import 'package:webblen/services_general/service_page_transitions.dart';
-import 'package:webblen/models/webblen_user.dart';
 import 'package:webblen/models/community.dart';
+import 'package:webblen/models/webblen_user.dart';
+import 'package:webblen/services_general/service_page_transitions.dart';
+import 'package:webblen/styles/flat_colors.dart';
+import 'package:webblen/styles/fonts.dart';
 import 'package:webblen/widgets_common/common_appbar.dart';
+import 'package:webblen/widgets_common/common_button.dart';
 
 class ChoosePostTypePage extends StatelessWidget {
-
   final WebblenUser currentUser;
   final Community community;
   ChoosePostTypePage({this.currentUser, this.community});
 
   @override
   Widget build(BuildContext context) {
-
-    Widget optionsButton(String buttonName, VoidCallback onPressed){
+    Widget optionsButton(String buttonName, VoidCallback onPressed) {
       return CustomColorButton(
         text: buttonName,
         textColor: FlatColors.darkGray,
@@ -40,14 +38,16 @@ class ChoosePostTypePage extends StatelessWidget {
                 child: Fonts().textW700("What Would You Like to Add?", 20.0, FlatColors.darkGray, TextAlign.center),
               ),
               SizedBox(height: 24.0),
-              optionsButton("Special Event", () => PageTransitionService(context: context, currentUser: currentUser, community: community, isRecurring: false).transitionToNewEventPage()),
-              optionsButton("Regular/Repeating Event", () => PageTransitionService(context: context, currentUser: currentUser, community: community).transitionToNewRecurringEventPage()),
-              optionsButton("News Post", () => PageTransitionService(context: context, currentUser: currentUser, community: community).transitionToCommunityCreatePostPage()),
+              optionsButton(
+                  "Special Event",
+                  () => PageTransitionService(context: context, currentUser: currentUser, community: community, isRecurring: false)
+                      .transitionToCreateEditEventPage()),
+              optionsButton("Regular/Repeating Event",
+                  () => PageTransitionService(context: context, currentUser: currentUser, community: community).transitionToNewRecurringEventPage()),
+              optionsButton("News Post",
+                  () => PageTransitionService(context: context, currentUser: currentUser, community: community).transitionToCommunityCreatePostPage()),
             ],
-          )
-      ),
+          )),
     );
   }
 }
-
-
