4fbf01db1d79bcc368acbd12638901652f352a56
Mukai Selekwa <mukaiss@outlook.com>
2020-01-01 17:00:07 -0600

    Update friends_page.dart

--- lib/user_pages/friends_page.dart
+++ lib/user_pages/friends_page.dart
@@ -1,19 +1,18 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:webblen/firebase_data/user_data.dart';
 import 'package:webblen/models/webblen_user.dart';
-import 'package:webblen/widgets_user/user_row.dart';
-import 'package:webblen/styles/fonts.dart';
 import 'package:webblen/services_general/service_page_transitions.dart';
 import 'package:webblen/styles/flat_colors.dart';
+import 'package:webblen/styles/fonts.dart';
 import 'package:webblen/widgets_common/common_progress.dart';
-import 'package:webblen/firebase_data/user_data.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'dart:async';
 import 'package:webblen/widgets_data_streams/stream_user_data.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'package:webblen/widgets_user/user_row.dart';
 
 class FriendsPage extends StatefulWidget {
-
   final String uid;
   FriendsPage({this.uid});
 
@@ -22,14 +21,12 @@ class FriendsPage extends StatefulWidget {
 }
 
 class _FriendsPageState extends State<FriendsPage> with SingleTickerProviderStateMixin {
-
   bool showLoadingDialog;
   StreamSubscription userStream;
   WebblenUser currentUser;
   bool loadingFriends = true;
   bool loadingRequests = true;
   List<WebblenUser> friendList = [];
-
 
   @override
   void initState() {
@@ -38,16 +35,16 @@ class _FriendsPageState extends State<FriendsPage> with SingleTickerProviderStat
   }
 
   initialize() async {
-    StreamUserData.getUserStream(widget.uid, getUser).then((StreamSubscription<DocumentSnapshot> s){
+    StreamUserData.getUserStream(widget.uid, getUser).then((StreamSubscription<DocumentSnapshot> s) {
       userStream = s;
     });
   }
 
-  getUser(WebblenUser user){
+  getUser(WebblenUser user) {
     currentUser = user;
-    if (currentUser != null){
+    if (currentUser != null) {
       List friendIDs = currentUser.friends;
-      if (friendIDs.isNotEmpty){
+      if (friendIDs.isNotEmpty) {
         getFriends(friendIDs);
       } else {
         loadingFriends = false;
@@ -57,39 +54,37 @@ class _FriendsPageState extends State<FriendsPage> with SingleTickerProviderStat
   }
 
   getFriends(List friendIDs) async {
-    UserDataService().getUsersFromList(currentUser.friends).then((result){
+    UserDataService().getUsersFromList(currentUser.friends).then((result) {
       friendList = result;
       friendList.sort((userA, userB) => userA.username.compareTo(userB.username));
       loadingFriends = false;
-      if (this.mounted){
+      if (this.mounted) {
         setState(() {});
       }
     });
   }
 
-  Widget buildFriendsView(){
+  Widget buildFriendsView() {
     return loadingFriends
         ? LoadingScreen(context: context, loadingDescription: 'Loading...')
         : Container(
-      height: MediaQuery.of(context).size.height * 0.88,
-      child: friendList.length == 0
-          ? buildEmptyListView("You Currently Have No Friends. Go Out to Events and Makes Some!", "desert")
-          : ListView.builder(
-          shrinkWrap: true,
-          itemBuilder: (context, index) =>
-              UserRow(
-                  user: friendList[index],
-                  transitionToUserDetails: () => PageTransitionService(context: context, currentUser: currentUser, webblenUser: friendList[index]).transitionToUserDetailsPage(),
-                  sendUserFriendRequest: null,
-                  isFriendsWithUser: true
-              ),
-          itemCount: friendList.length ,
-      ),
-    );
+            height: MediaQuery.of(context).size.height * 0.88,
+            child: friendList.length == 0
+                ? buildEmptyListView("You Currently Have No Friends. Go Out to Events and Makes Some!", "desert")
+                : ListView.builder(
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) => UserRow(
+                        user: friendList[index],
+                        transitionToUserDetails: () =>
+                            PageTransitionService(context: context, currentUser: currentUser, webblenUser: friendList[index]).transitionToUserDetailsPage(),
+                        sendUserFriendRequest: null,
+                        isFriendsWithUser: true),
+                    itemCount: friendList.length,
+                  ),
+          );
   }
 
-
-  Widget buildEmptyListView(String emptyCaption, String pictureName){
+  Widget buildEmptyListView(String emptyCaption, String pictureName) {
     return Container(
       margin: EdgeInsets.all(16.0),
       width: MediaQuery.of(context).size.width,
@@ -103,29 +98,31 @@ class _FriendsPageState extends State<FriendsPage> with SingleTickerProviderStat
     );
   }
 
-
   @override
   Widget build(BuildContext context) {
-
-    final appBar = AppBar (
+    final appBar = AppBar(
       elevation: 0.5,
       brightness: Brightness.light,
       backgroundColor: Color(0xFFF9F9F9),
-      title: Text('Friends', style: new TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600, color: FlatColors.blackPearl)),
-      leading: BackButton(color: FlatColors.londonSquare),
+      title: Text('Friends', style: new TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600, color: Colors.black)),
+      leading: BackButton(color: Colors.black),
       actions: <Widget>[
         IconButton(
-          icon: Icon(FontAwesomeIcons.search, size: 20.0, color: Colors.black45),
-          onPressed: () => PageTransitionService(context: context, usersList: friendList, currentUser: currentUser, viewingMembersOrAttendees: false).transitionToUserSearchPage(),
+          icon: Icon(FontAwesomeIcons.search, size: 20.0, color: Colors.black),
+          onPressed: () => PageTransitionService(context: context, usersList: friendList, currentUser: currentUser, viewingMembersOrAttendees: false)
+              .transitionToUserSearchPage(),
         )
       ],
     );
 
-    return Scaffold(
-      appBar: appBar,
-      body: Container(
-          child:  buildFriendsView(),
+    return MediaQuery(
+      data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+      child: Scaffold(
+        appBar: appBar,
+        body: Container(
+          child: buildFriendsView(),
         ),
+      ),
     );
   }
 }
