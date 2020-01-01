84d11ed09a4b50cdda81b2f4ae0bb6eced1ea207
Mukai Selekwa <mukaiss@outlook.com>
2020-01-01 17:19:58 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,21 @@
 import 'package:flutter/material.dart';
-import 'auth_pages/login_page.dart';
-import 'package:webblen/user_pages/setup_page.dart';
 import 'package:flutter/services.dart';
+import 'package:webblen/user_pages/setup_page.dart';
+
+import 'auth_pages/login_page.dart';
+import 'home_page.dart';
 import 'root_page.dart';
 import 'styles/flat_colors.dart';
-import 'home_page.dart';
-
 
 void main() => runApp(new WebblenApp());
 
 class WebblenApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-
     return MaterialApp(
       title: 'Webblen',
       debugShowCheckedModeBanner: false,
@@ -28,13 +25,12 @@ class WebblenApp extends StatelessWidget {
         accentColor: FlatColors.darkGray,
       ),
       home: RootPage(),
-      routes: <String, WidgetBuilder> {
+      routes: <String, WidgetBuilder>{
         '/home': (BuildContext context) => new HomePage(),
         '/root': (BuildContext context) => new RootPage(),
-        '/login' : (BuildContext context) => new LoginPage(),
-        '/setup' : (BuildContext context) => new SetupPage(),
+        '/login': (BuildContext context) => new LoginPage(),
+        '/setup': (BuildContext context) => new SetupPage(),
       },
     );
   }
 }
-
