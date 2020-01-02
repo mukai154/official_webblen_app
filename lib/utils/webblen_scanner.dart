55e52963e2c1dda4258db9c004fca1828c0b1232
mukai154 <mukaiss@outlook.com>
2020-01-01 19:55:20 -0600

    8.0.0-fix

--- lib/utils/webblen_scanner.dart
+++ lib/utils/webblen_scanner.dart
@@ -1,206 +1,87 @@
-import 'dart:async';
-import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:webblen/firebase_data/webblen_notification_data.dart';
+import 'package:webblen/models/webblen_user.dart';
+import 'package:webblen/services_general/services_show_alert.dart';
+import 'package:webblen/widgets_common/common_appbar.dart';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cloud_functions/cloud_functions.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:random_string/random_string.dart';
-import 'package:webblen/firebase_data/user_data.dart';
-import 'package:webblen/models/webblen_notification.dart';
+class WebblenScanner extends StatefulWidget {
+  final WebblenUser currentUser;
 
-class WebblenNotificationDataService {
-  final CollectionReference notifRef = Firestore.instance.collection("user_notifications");
-  final StorageReference storageReference = FirebaseStorage.instance.ref();
+  WebblenScanner({this.currentUser});
 
-  //GENERAL
-  Future<Null> updateNotificationStatus(String notifKey) async {
-    notifRef.document(notifKey).updateData(({'notificationSeen': true}));
-  }
+  @override
+  _WebblenScannerState createState() => _WebblenScannerState();
+}
 
-  Future<Null> deleteNotification(String notifKey) async {
-    notifRef.document(notifKey).delete();
-  }
+class _WebblenScannerState extends State<WebblenScanner> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  String dataType = "";
+  String data = "";
+  String additionalData = "";
+  QRViewController controller;
 
-  Future<List<WebblenNotification>> getUserNotifications(String uid) async {
-    List<WebblenNotification> notifs = [];
-    final HttpsCallable callable = CloudFunctions.instance.getHttpsCallable(functionName: 'getUserNotifications');
-    final HttpsCallableResult result = await callable.call(<String, dynamic>{'uid': uid});
-    if (result.data != null) {
-      List query = List.from(result.data);
-      query.forEach((resultMap) {
-        Map<String, dynamic> notifMap = Map<String, dynamic>.from(resultMap);
-        WebblenNotification notif = WebblenNotification.fromMap(notifMap);
-        notifs.add(notif);
-      });
-    }
-    return notifs;
-  }
-
-  //EVENTS
-  Future<String> shareEventWithFriend(String receivingUID, String senderUID, String senderName, String eventKey, String eventTitle) async {
-    String error;
-    String notifDescription;
-    String notifKey = randomAlphaNumeric(10);
-    int ranNum = Random().nextInt(5);
-    if (ranNum == 0) {
-      notifDescription = "@$senderName thinks this is something you should checkout 👀";
-    } else if (ranNum == 1) {
-      notifDescription = "@$senderName shared an event with you!";
-    } else if (ranNum == 2) {
-      notifDescription = "@$senderName knows this event would be 10x better if you came";
-    } else if (ranNum == 3) {
-      notifDescription = "@$senderName would love if you came here️";
-    } else if (ranNum == 4) {
-      notifDescription = "@$senderName doesn't want you to miss this...";
-    } else {
-      notifDescription = "@$senderName thinks this is something worth looking into";
-    }
-    WebblenNotification notification = WebblenNotification(
-        notificationData: eventKey,
-        notificationDescription: notifDescription,
-        notificationExpDate: DateTime.now().add(Duration(days: 14)).millisecondsSinceEpoch,
-        notificationTitle: eventTitle,
-        notificationExpirationDate: DateTime.now().add(Duration(days: 14)).millisecondsSinceEpoch.toString(),
-        notificationKey: notifKey,
-        notificationSeen: false,
-        notificationSender: senderName,
-        notificationType: "eventShare",
-        sponsoredNotification: false,
-        uid: receivingUID,
-        messageToken: "");
-    notifRef.document(notifKey).setData(notification.toMap()).whenComplete(() {}).catchError((e) {
-      error = e.details;
-    });
-    return error;
-  }
-
-  //Community Invitations
-  Future<bool> checkIfComInviteExists(String areaName, String comName, String receivingUid) async {
-    bool exists = false;
-    String modifiedComName = comName.contains("#") ? comName : "#$comName";
-    String comNotifData = '$areaName.$modifiedComName';
-    await notifRef
-        .where('notificationData', isEqualTo: comNotifData)
-        .where('uid', isEqualTo: receivingUid)
-        .where('notificationType', isEqualTo: 'invite')
-        .getDocuments()
-        .then((result) {
-      if (result.documents != null && result.documents.length > 0) {
-        exists = true;
-      }
-    });
-    return exists;
-  }
-
-  Future<String> sendCommunityInviteNotif(String senderUid, String areaName, String comName, String receivingUid, String notifDescription) async {
-    String status = "";
-    String modifiedComName = comName.contains("#") ? comName : "#$comName";
-    String comNotifData = '$areaName.$modifiedComName';
-    String notifKey = Random().nextInt(999999999).toString();
-    String messageToken = await UserDataService().findUserMesseageTokenByID(receivingUid);
-    WebblenNotification notification = WebblenNotification(
-        notificationData: comNotifData,
-        notificationDescription: notifDescription,
-        notificationExpDate: DateTime.now().add(Duration(days: 14)).millisecondsSinceEpoch,
-        notificationTitle: "",
-        notificationExpirationDate: DateTime.now().add(Duration(days: 14)).millisecondsSinceEpoch.toString(),
-        notificationKey: notifKey,
-        notificationSeen: false,
-        notificationSender: senderUid,
-        notificationType: "invite",
-        sponsoredNotification: false,
-        uid: receivingUid,
-        messageToken: messageToken);
-
-    notifRef.document(notifKey).setData(notification.toMap()).whenComplete(() {}).catchError((e) {
-      status = e.details;
-    });
-    return status;
-  }
-
-  Future<bool> acceptCommunityInvite(String areaName, String comName, String uid, String notifKey) async {
-    bool success = false;
-    final HttpsCallable callable = CloudFunctions.instance.getHttpsCallable(functionName: 'acceptCommunityInvite');
-    final HttpsCallableResult result = await callable.call(<String, dynamic>{'areaName': areaName, 'comName': comName, 'uid': uid, 'notifKey': notifKey});
-    if (result.data != null) {
-      success = result.data;
-    }
-    return success;
-  }
-
-  //Friend Requests
-  Future<bool> checkIfFriendRequestExists(String senderUID, String receivingUid) async {
-    bool exists = false;
-    await notifRef.where('notificationData', isEqualTo: senderUID).where('uid', isEqualTo: receivingUid).getDocuments().then((result) {
-      if (result.documents != null && result.documents.length > 0) {
-        exists = true;
-      }
-    });
-    return exists;
-  }
-
-  Future<String> sendFriendRequest(String uid, String peerUID, String username) async {
-    String error = "";
-    String notifKey = Random().nextInt(999999999).toString();
-    String messageToken = await UserDataService().findUserMesseageTokenByID(peerUID);
-    WebblenNotification notification = WebblenNotification(
-      messageToken: messageToken,
-      notificationData: uid,
-      notificationTitle: "",
-      notificationExpDate: DateTime.now().add(Duration(days: 14)).millisecondsSinceEpoch,
-      notificationDescription: "@$username wants to be your friend",
-      notificationExpirationDate: DateTime.now().add(Duration(days: 14)).millisecondsSinceEpoch.toString(),
-      notificationKey: notifKey,
-      notificationSeen: false,
-      notificationSender: username,
-      notificationType: "friendRequest",
-      sponsoredNotification: false,
-      uid: peerUID,
-    );
-    notifRef.document(notifKey).setData(notification.toMap()).whenComplete(() {}).catchError((e) {
-      error = e.details;
-    });
-    return error;
-  }
-
-  Future<bool> acceptFriendRequest(String uid, String friendUid, String notifKey) async {
-    bool success = false;
-    String key = notifKey;
-    if (notifKey == null) {
-      notifKey = "";
-    }
-    final HttpsCallable callable = CloudFunctions.instance.getHttpsCallable(functionName: 'acceptFriendRequest');
-    final HttpsCallableResult result = await callable.call(<String, dynamic>{'receiverUID': uid, 'requesterUID': friendUid, 'notifKey': key});
-    if (result.data != null) {
-      success = result.data;
-    }
-    return success;
-  }
-
-  Future<bool> denyFriendRequest(String uid, String friendUid, String notifKey) async {
-    bool success = false;
-    String key = notifKey;
-    if (notifKey == null) {
-      notifKey = "";
-    }
-    final HttpsCallable callable = CloudFunctions.instance.getHttpsCallable(functionName: 'denyFriendRequest');
-    final HttpsCallableResult result = await callable.call(<String, dynamic>{'receiverUID': uid, 'requesterUID': friendUid, 'notifKey': key});
-    if (result.data != null) {
-      success = result.data;
-    }
-    return success;
-  }
-
-  //Posts
-  Future<Null> deletePostNotifications(String postTitle, String areaName, String comName) async {
-    String modifiedComName = comName.contains("#") ? comName : "#$comName";
-    String comNotifData = '$areaName.$modifiedComName';
-    await notifRef.where('notificationDescription', isEqualTo: postTitle).where('notificationData', isEqualTo: comNotifData).getDocuments().then((query) {
-      if (query.documents != null && query.documents.length > 0) {
-        query.documents.forEach((doc) {
-          notifRef.document(doc.documentID).delete();
+  void sendFriendRequest(String uid, String username) async {
+    Navigator.of(context).pop();
+    ShowAlertDialogService().showLoadingDialog(context);
+    WebblenNotificationDataService().checkIfFriendRequestExists(widget.currentUser.uid, uid).then((exists) {
+      if (exists) {
+        Navigator.of(context).pop();
+        ShowAlertDialogService().showFailureDialog(context, "Friend Request Pending", "You Already Have a Pending Request with $username");
+      } else {
+        WebblenNotificationDataService().sendFriendRequest(widget.currentUser.uid, uid, widget.currentUser.username).then((error) {
+          Navigator.of(context).pop();
+          if (error.isEmpty) {
+            ShowAlertDialogService().showActionSuccessDialog(context, "Friend Request Sent!", username + " Will Need to Confirm Your Request", () {
+              Navigator.of(context).pop();
+              Navigator.of(context).pop();
+            });
+          } else {
+            ShowAlertDialogService().showFailureDialog(context, "Request Failed", error);
+          }
         });
       }
     });
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      //print(scanData);
+      controller.pauseCamera();
+      if (!scanData.contains("addFriend.")) {
+        ShowAlertDialogService().showFailureDialog(context, "That's Odd", "We Don't Recongnize this Code...");
+      } else {
+        int dataSplitterIndex = scanData.indexOf(".", 0);
+        int dataSplitterIndex2 = scanData.indexOf("-", 0);
+        dataType = scanData.substring(0, dataSplitterIndex);
+        data = scanData.substring(dataSplitterIndex + 1, dataSplitterIndex2);
+        additionalData = scanData.substring(dataSplitterIndex2 + 1, scanData.length);
+        if (data == widget.currentUser.uid) {
+          ShowAlertDialogService().showFailureDialog(context, "That's Odd", "This is Your Account...");
+        } else if (dataType == "addFriend") {
+          String uid = data;
+          String username = "@" + additionalData;
+          ShowAlertDialogService().showScannedAccount(context, username, uid, () => sendFriendRequest(uid, username));
+        }
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: WebblenAppBar().basicAppBar('Scan Code', context),
+      body: QRView(
+        key: qrKey,
+        onQRViewCreated: _onQRViewCreated,
+      ),
+    );
   }
 }
